cb1633da0466885c243cec2a871f5c70b81012f7
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-06 06:58:00 -0400

    starter commit

---
+++ micro_blog/src/app/page.js
@@ -0,0 +1,18 @@
+import Featured from "@/components/Featured/Featured";
+import styles from "./homepage.module.css";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import CardList from "@/components/Card/CardList/CardList";
+import Menu from "@/components/Menu/Menu/Menu";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+    <div className={styles.content}>
+      <CardList />
+      <Menu />
+    </div>
+    </div>
+  );
+}
