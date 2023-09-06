734009f8c4d58d395ff8960cd2fcdaa8a3f62ab7
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-06 08:38:10 -0400

    card / menu

--- micro_blog/src/components/Card/Card/Card.jsx
+++ micro_blog/src/components/Card/Card/Card.jsx
@@ -1,9 +1,40 @@
 import styles from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = () => {
   return (
-    <div className={styles.container}>
-      Card
+    // <div className={styles.container} key={key}>
+      <div className={styles.container}>
+      {/* {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )} */}
+      <Image src="" alt="" fill className={styles.image} />
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>
+            {/* {item.createdAt.substring(0, 10)} -{" "} */}
+            Date 
+          </span>
+          {/* <span className={styles.category}>{item.catSlug}</span> */}
+          <span className={styles.category}> Category</span>
+        </div>
+        {/* <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1> */}
+        <Link href="">
+          <h1>Title</h1>
+        </Link>
+        {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+        {/* <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/> */}
+        <div className={styles.desc} />
+        Body
+        {/* <Link href={`/posts/${item.slug}`} className={styles.link}> */}
+        <Link href="" className={styles.link}>
+          Read More
+        </Link>
+      </div>
     </div>
   )
 }
--- micro_blog/src/components/Menu/Menu/Menu.jsx
+++ micro_blog/src/components/Menu/Menu/Menu.jsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import MenuCategories from "../MenuCategories/MenuCategories";
+import MenuPosts from "../MenuPosts/MenuPosts";
 import styles from "./menu.module.css";
+import Link from "next/link";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
-      Menu
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
   )
 }
