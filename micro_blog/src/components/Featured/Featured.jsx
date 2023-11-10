a802f7dec032cd7d753aeb8f061a39764b5ce290
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-11-09 19:02:26 -0500

    update schema

--- micro_blog/src/components/Featured/Featured.jsx
+++ micro_blog/src/components/Featured/Featured.jsx
@@ -4,9 +4,6 @@ import Image from "next/image";
 const Featured = () => {
   return (
     <div className={styles.container}>
-      {/* <h1 className={styles.title}>
-        <b>Hey, Kab here!</b> Discover my stories and creative ideas.
-      </h1> */}
       <div className={styles.post}>
         <div className={styles.imgContainer}>
           <Image src="/p1.jpeg"
